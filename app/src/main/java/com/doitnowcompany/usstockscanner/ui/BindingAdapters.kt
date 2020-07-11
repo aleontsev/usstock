c7cc4d8095151b6556abba4f71bbe67f2e178d35
hermes <aleontsev@gmail.com>
2020-07-11 12:21:55 +0300

    1.Applied data binding for Recicler View and ScanButton. 2.Made preparings for chart loading.

---
+++ app/src/main/java/com/doitnowcompany/usstockscanner/ui/BindingAdapters.kt
@@ -0,0 +1,26 @@
+package com.doitnowcompany.usstockscanner.ui
+
+import android.widget.ImageView
+import androidx.core.net.toUri
+import androidx.databinding.BindingAdapter
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.doitnowcompany.usstockscanner.R
+
+/**
+ * Uses the Glide library to load an image by URL into an [ImageView]
+ */
+
+@BindingAdapter("chartUrl")
+fun bindImage(imgView: ImageView, imgUrl: String?) {
+    imgUrl?.let {
+        val imgUri = imgUrl.toUri().buildUpon().scheme("https").build()
+        Glide.with(imgView.context)
+            .load(imgUri)
+            .apply(
+                RequestOptions()
+                .placeholder(R.drawable.loading_animation)
+                .error(R.drawable.ic_broken_image))
+            .into(imgView)
+    }
+}
--- app/src/main/java/com/doitnowcompany/usstockscanner/ui/MainActivity.kt
+++ app/src/main/java/com/doitnowcompany/usstockscanner/ui/MainActivity.kt
@@ -1,10 +1,10 @@
 package com.doitnowcompany.usstockscanner.ui
 
-import android.annotation.SuppressLint
-import android.graphics.drawable.ColorDrawable
+
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
+import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.DividerItemDecoration
@@ -12,21 +12,21 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import com.doitnowcompany.usstockscanner.R
 import com.doitnowcompany.usstockscanner.databinding.ActivityMainBinding
 import com.doitnowcompany.usstockscanner.viewmodel.TickerListViewModel
-import java.security.AccessController.getContext
 
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var tickerViewModel: TickerListViewModel
-    private lateinit var binding: ActivityMainBinding
+    // Get a new or existing ViewModel from the ViewModelProvider.
+    private  val tickerViewModel by lazy {ViewModelProvider(this).get(TickerListViewModel::class.java)}
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        val view = binding.root
         super.onCreate(savedInstanceState)
-        setContentView(view)
 
-        val scan = binding.scan
+        val binding : ActivityMainBinding =
+            DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.viewmodel = tickerViewModel
+        binding.lifecycleOwner = this
+
         val recyclerView = binding.recyclerview
         val adapter = TickerListAdapter(this)
         recyclerView.adapter = adapter
@@ -41,12 +41,7 @@ class MainActivity : AppCompatActivity() {
             )
         };
 
-
         recyclerView.addItemDecoration(mDividerItemDecoration)
-
-
-        // Get a new or existing ViewModel from the ViewModelProvider.
-        tickerViewModel = ViewModelProvider(this).get(TickerListViewModel::class.java)
 
         // Add an observer on the LiveData returned by getAlphabetizedTickers.
         // The onChanged() method fires when the observed data changes and the activity is
@@ -55,11 +50,6 @@ class MainActivity : AppCompatActivity() {
             // Update the cached copy of the tickers in the adapter.
             tickers?.let { adapter.setTickers(it) }
         })
-
-        // When scan is clicked tickers loaded from network
-        scan.setOnClickListener{
-            tickerViewModel.refreshTickers()
-        }
 
     }
 }
